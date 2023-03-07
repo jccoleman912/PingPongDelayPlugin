60f244875a8ae931434d6a878f1c69cf00b349d5
Jackson Coleman <jackdonsurfer@Jacksons-MacBook-Pro.local>
2023-03-06 20:55:23 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# HomeworkExample
+Jackson Coleman
+March 6 2023
+Repository for Audio Software Development Homework 2 assignment.
---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class Coleman_HW2AudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    Coleman_HW2AudioProcessorEditor (Coleman_HW2AudioProcessor&);
+    ~Coleman_HW2AudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    Coleman_HW2AudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Coleman_HW2AudioProcessorEditor)
+};
